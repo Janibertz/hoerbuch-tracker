21bba269f2b19165701bcd7377c596598ef8d603
Janibertz <jan.anders@me.com>
2025-05-14 10:36:32 +0200

    Bugfix

--- app/Http/Controllers/SpotifyPlaylistController.php
+++ app/Http/Controllers/SpotifyPlaylistController.php
@@ -106,7 +106,7 @@ class SpotifyPlaylistController extends Controller
 
 public function refresh(\App\Models\Playlist $playlist): int
 {
-    $token = \App\Services\SpotifyTokenService::getValidAccessToken();
+    $token = SpotifyTokenService::getValidAccessToken();
 
     if (!$token) {
         return 0;
